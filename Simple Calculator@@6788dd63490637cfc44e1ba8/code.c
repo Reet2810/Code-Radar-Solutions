603665720bd314a54b45de303e4756cff9ae8817
Reet Arora <reetarora2810@gmail.com>
2025-02-18 03:22:05 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    char choice;
+    scanf("%d %d %c", &a, &b, &choice);
+
+    switch(choice){
+        case '+':
+            printf("%d", a+b);
+            break;
+        case '-':
+            printf("%d", a-b);
+            break;
+        case'*':
+            printf("%d", a*b);
+            break;
+        case'/':
+            printf("%d", a/b);
+            break;
+    }
+
+    return 0;
+}
